c199108ea5e64688e65b3bd5a73b8722769ca0d4
RameshXt <rameshxt.1@gmail.com>
2025-04-16 12:21:02 +0530

    Fix: permission denied for image updater bash

--- image-updater.sh
+++ image-updater.sh
@@ -3,7 +3,7 @@
 IMAGE_TAG=$1
 
 # IMAGE UPDATER
-echo "Updating deployment.yam with the new image tag: $IMAGE_TAG"
-sed -i "s|image\s*=\s*\".*\"|image = \"$IMAGE_TAG\"|" k8s/deployment.yaml
+echo "Updating deployment.yaml with the new image tag: $IMAGE_TAG"
+sed -i "s|image: .*|image: $IMAGE_TAG|" k8s/deployment.yaml
 
 echo "deployment.yaml file updated successfully with image tag: $IMAGE_TAG"
